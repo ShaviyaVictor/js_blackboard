41fb87a722eec27363186bb4158351eb682ad902
Victor Shaviya <vs610642@digicelgroup.com>
2022-07-24 18:14:29 -0500

    Declare the function variables

--- assets/logic.js
+++ assets/logic.js
@@ -1,3 +1,8 @@
 document.addEventListener('mousemove', function(e) {
-  
+
+  let body = document.querySelector('body');
+  let particles = document.createElement('span');
+  let x = e.offsetX;
+  let y = e.offsetY;
+
 })
