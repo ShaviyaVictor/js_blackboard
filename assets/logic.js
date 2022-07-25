bf8dad9179c576e17a201bbffbb24e55b272b3b8
Victor Shaviya <vs610642@digicelgroup.com>
2022-07-24 19:02:07 -0500

    Create a function that rotates the particles randomly

--- assets/logic.js
+++ assets/logic.js
@@ -12,6 +12,10 @@ document.addEventListener('mousemove', function(e) {
   particles.style.width = 2 + size + 'px';
   particles.style.height = 2 + size + 'px';
 
+  let transformValue = Math.random() * 360;
+  particles.style.transform = 'rotate('
+    + transformValue + 'deg)';
+
   body.appendChild(particles);
 
   setTimeout(function() {
